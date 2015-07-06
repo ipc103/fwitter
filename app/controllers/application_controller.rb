c3fc36c8d6e87eb3f0c45f877be86744c6339f01
Ian Candy <iancandysemail@gmail.com>
2015-07-06 19:53:18 +0000

    update configuration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ require_relative "../../config/environment"
 require_relative "../models/fweet.rb"
 class ApplicationController < Sinatra::Base
   
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+  
   get "/" do
     "Hello World"
   end
